4eda424e7efe03749ea316f89990d104d868b0ee
HongC0126 <112526769+HongC0126@users.noreply.github.com>
2026-04-26 22:55:52 -0500

    Update en.js
    change basic information about myself

--- lang/en.js
+++ lang/en.js
@@ -10,21 +10,23 @@ const enNavbarData = {
 
 // ---------- Home page data
 const enHomePageData = {
-  name: " Your name ",
-  jobTitle: " Job title ",
+  name: " Hong(Cindy) Chen ",
+  jobTitle: " Research Technician ",
   home_title: "About me",
   home_content: `
   <div>
-    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.
-      ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    <p> With a strong interest in Information Technology, I earned my Master of Information Systems Management in December 2023. During my first internship at China Unicom, I focused on developing unit tests using Python (Pytest) and contributed to CI/CD pipeline development.
+Since August 2023, I have been involved in building unit tests for the NetworkAnalysis application and supporting early-stage development of a sleep stage classification project. Beginning in January 2024, I transitioned to an imaging science–related role, where I worked on data transfer, data cleaning, and model training.
+Since January 2025, I have been working with aging datasets, analyzing brain anatomical surface changes, as well as contributing to clinical studies involving multimodal EEG and fMRI data from infants and toddlers.
     </p>
     <h2 class='title'> Main works </h2>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas purus viverra accumsan in nisl nisi. Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque. In egestas erat imperdiet sed euismod nisi porta lorem mollis. Morbi tristique senectus et netus. Mattis pellentesque id nibh  </p>
     <ul>
-      <li> Heuristics</li>
-      <li> Policy planning </li>
-      <li> Flexiblity to details </li>
+      <li> fMRI</li>
+      <li> Aging</li>
+      <li> Alzheimer's </li>
+      <li> Brain </li>
+      <li> Sleep </li>
     </ul>
   </div>
   `,
